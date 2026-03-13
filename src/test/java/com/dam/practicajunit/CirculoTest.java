3e76bf72ad7ed0455e5224644b0db87b3dfd9779
Jhonatan <jhonatancucunamelopez@gmail.com>
2026-03-13 17:52:41 +0100

    Edicion del Pom.xml para hacer las pruebas y test de constructor vacio

---
+++ src/test/java/com/dam/practicajunit/CirculoTest.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.dam.practicajunit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author jhonatan.cucunamelop
+ */
+public class CirculoTest {
+    
+    public CirculoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    
+    @Test
+    public void testCirculo(){
+        System.out.println("A");
+        Circulo instancia = new Circulo();
+        double resultadoEsperado = 0;
+        double resultado = instancia.getRadio();
+        assertEquals(resultadoEsperado,resultado, 0);
+        
+    }
+    
+    
+    /**
+     * Test of calcularArea method, of class Circulo.
+     */
+    @Test
+    public void testCalcularArea() {
+        System.out.println("calcularArea");
+        Circulo instance = new Circulo();
+        double expResult = 0.0;
+        double result = instance.calcularArea();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularPerimetro method, of class Circulo.
+     */
+    @Test
+    public void testCalcularPerimetro() {
+        System.out.println("calcularPerimetro");
+        Circulo instance = new Circulo();
+        double expResult = 0.0;
+        double result = instance.calcularPerimetro();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+       fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRadio method, of class Circulo.
+     */
+    @Test
+    public void testGetRadio() {
+        System.out.println("getRadio");
+        Circulo instance = new Circulo();
+        double expResult = 0.0;
+        double result = instance.getRadio();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRadio method, of class Circulo.
+     */
+    @Test
+    public void testSetRadio() {
+        System.out.println("setRadio");
+        double radio = 0.0;
+        Circulo instance = new Circulo();
+        instance.setRadio(radio);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mostrarNombre method, of class Circulo.
+     */
+    @Test
+    public void testMostrarNombre() {
+        System.out.println("mostrarNombre");
+        Circulo instance = new Circulo();
+        instance.mostrarNombre();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mostrarRadio method, of class Circulo.
+     */
+    @Test
+    public void testMostrarRadio() {
+        System.out.println("mostrarRadio");
+        Circulo circulito = null;
+        double expResult = 0.0;
+        double result = Circulo.mostrarRadio(circulito);
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
